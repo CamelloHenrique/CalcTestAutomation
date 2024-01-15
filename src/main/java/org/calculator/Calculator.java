42ae393924ed0271ed614c64026ec071d24869c9
Henrique Camello <henrique.camello@zumpy.com.br>
2024-01-15 14:33:18 -0300

    first commit

---
+++ src/main/java/org/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package org.calculator;
+
+public class Calculator {
+    public static int add(int number1, int number2){
+        return number1+number2;
+    }
+
+    public static int subtract(int number1, int number2){
+        return number1-number2;
+    }
+
+    public static int divide(int number1, int number2){
+        return number1/number2;
+    }
+
+    public static int multiply(int number1, int number2){
+        return number1*number2;
+    }
+}
---
+++ src/test/java/org/calculator/CalculatorTest.java
@@ -0,0 +1,33 @@
+package org.calculator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private Calculator calc;
+
+    @Before
+    public void before() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(7,calc.add(3,4));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(3,calc.subtract(5,2));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(10,calc.divide(60,6));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(12,calc.multiply(3,4));
+    }
+}
